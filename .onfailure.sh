1c7ab74c2c1d9ae2e858b4beac59433bb152257e
Jason Lamb <jlamb@lambcorp.com>
2020-08-20 10:57:16 -0400

    Create .onfailure.sh

---
+++ .onfailure.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+VPS=$1
+Service=$2
+
+MN=$(systemctl | grep "$Service" | head -1 | awk '{ print $NF }')
+
+Message="ALERT: On $VPS, $Service, $MN FAILED"
+
+curl -X POST -H 'Content-type: application/json' --data '{"text":"'"$Message"'"}' https://hooks.slack.com/services/T013XQUDZB5/B014N8CRZDF/J2t0fkUSIEVd7kX$
