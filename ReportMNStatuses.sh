f69d1bc70665e8552851268843fc4bcab5ab628a
Jason Lamb <jlamb@lambcorp.com>
2020-09-12 13:01:30 -0400

    Update ReportMNStatuses.sh

--- ReportMNStatuses.sh
+++ ReportMNStatuses.sh
@@ -79,11 +79,10 @@ do
       echo "" | tee -a $StatusReportFilename
 
       if ! [[ $tempStatus == "ENABLED" ]]; then
-        echo "Uh OH!!"
-        Message="ALERT: On $tempLabel, test, Status: $tempStatus"
 
-        curl -X POST -H 'Content-type: application/json' --data '{"text":"'"$Message"'"}' https://hooks.slack.com/services/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
+        Message="ALERT: On $tempLabel, $i, Status: $tempStatus"
 
+        curl -X POST --data-urlencode "payload={\"channel\": \"#jl_mns-monitor-blockchainstatus\", \"username\": \"MN_monitor\", \"text\": \"$Message\", \"icon_emoji\": \":wrench:\"}" https://hooks.slack.c$
       fi
 
 
