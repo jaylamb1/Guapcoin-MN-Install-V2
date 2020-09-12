f0d5cb9d61715ff64e55b22ff09f70f4c57cb376
Jason Lamb <jlamb@lambcorp.com>
2020-09-12 12:30:13 -0400

    Update ReportMNStatuses.sh

--- ReportMNStatuses.sh
+++ ReportMNStatuses.sh
@@ -80,7 +80,7 @@ do
 
       if ! [[ $tempStatus == "ENABLED" ]]; then
         echo "Uh OH!!"
-        Message="ALERT: On $tempLabel, $i, Status: $tempStatus"
+        Message="ALERT: On $tempLabel, test, Status: $tempStatus"
 
         curl -X POST -H 'Content-type: application/json' --data '{"text":"'"$Message"'"}' https://hooks.slack.com/services/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
 
