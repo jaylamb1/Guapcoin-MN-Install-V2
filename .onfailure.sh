a687b09e71207f15e3bac006d66bcb584d9aa280
Jason Lamb <jlamb@lambcorp.com>
2020-09-12 19:40:06 -0400

    Update .onfailure.sh

--- .onfailure.sh
+++ .onfailure.sh
@@ -7,4 +7,4 @@ MN=$(systemctl | grep "$Service" | head -1 | awk '{ print $NF }')
 
 Message="*ALERT:* On $VPS, $Service, $MN FAILED"
 
-curl -X POST -H 'Content-type: application/json' --data '{"text":"'"$Message"'"}' https://hooks.slack.com/services/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
+curl -X POST -H 'Content-type: application/json' --data '{"text":"'"$Message"'"}' https://hooks.slack.com/services/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
