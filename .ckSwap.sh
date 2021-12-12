985b2ad7078b66071f609dc4195f974e8afb8f59
jaylamb1 <53414117+jaylamb1@users.noreply.github.com>
2021-12-12 12:49:31 -0600

    Update .ckSwap.sh

--- .ckSwap.sh
+++ .ckSwap.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 VPS=$(hostname)
-FreeSwap=$(free -m | awk 'END{print}' | cut -c 40-44)
+#FreeSwap=$(free -m | awk 'END{print}' | cut -c 40-44)
+FreeSwap=$(free -m | awk 'END{print}' | awk '{ print $NF }')
 Message="*ALERT:* On $VPS, only $FreeSwap MB od free swap left"
 
 if [[ "$FreeSwap" -lt 499 ]]; then
 
-  curl -X POST -H 'Content-type: application/json' --data '{"text":"'"$Message"'"}' 
+  curl -X POST -H 'Content-type: application/json' --data '{"text":"'"$Message"'"}'
 
 fi
