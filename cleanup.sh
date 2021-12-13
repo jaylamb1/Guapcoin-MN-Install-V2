a5ec05f856fedc9fda979003d9a3dc9b04ab7202
jaylamb1 <53414117+jaylamb1@users.noreply.github.com>
2021-12-13 08:37:09 -0600

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -2,6 +2,6 @@
 
 #Cleanup the /home/guapadmin/MN_Report and /home/guapadmin/MN_Status dirs by deleting all but the last 10 files
 
-find /home/guapadmin/MN_Status -type f -mtime +9 -exec rm -f {} \; 2>/dev/null
+cd /home/guapadmin/MN_Status && find /home/guapadmin/MN_Status -type f -mtime +9 -exec rm -f {} \; 2>/dev/null
 
-find /home/guapadmin/MN_Report -type f -mtime +9 -exec rm -f {} \; 2>/dev/null
+cd cd /home/guapadmin/MN_Report && find /home/guapadmin/MN_Report -type f -mtime +9 -exec rm -f {} \; 2>/dev/null
